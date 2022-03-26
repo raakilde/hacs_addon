1a693e47e8e43f111b117e7b5ca92b75b3becb8e
Raakilde <j.olesen@vindinggaard.dk>
2022-03-26 21:02:45 +0100

    Clean up

--- custom_components/ewii/pyewii/ewii.py
+++ custom_components/ewii/pyewii/ewii.py
@@ -150,69 +150,6 @@ class Ewii:
         """Convert string with ',' string float to float"""
         return float(fstr.replace(',', '.'))
 
-    # def _parse_result(self, result):
-    #     '''
-    #     Parse result from API call. This is a JSON dict.
-    #     '''
-    #     _LOGGER.debug(f"Parsing results")
-    #     parsed_result = {}
-
-    #     metering_data = {}
-    #     metering_data['year_start'] = result['AarStart']
-    #     metering_data['year_end']   = result['AarSlut']
-    #     for fl in result['ForbrugsLinjer']['TForbrugsLinje']:
-    #         metering_data['temp-forward'] = self._stof(fl['Tempfrem'])
-    #         metering_data['temp-return'] = self._stof(fl['TempRetur'])
-    #         metering_data['temp-exp-return'] = self._stof(fl['Forv_Retur'])
-    #         metering_data['temp-cooling'] = self._stof(fl['Afkoling'])
-    #         for reading in fl['TForbrugsTaellevaerk']:
-    #             unit = reading['Enhed_Txt']
-    #             if reading['IndexNavn'] == "ENG1":
-    #                 #_LOGGER.debug(f"Energy use unit is: {unit}")
-    #                 multiplier = 1
-    #                 if unit == "MWh":
-    #                     multiplier = 1000
-    #                 metering_data['energy-start'] = self._stof(reading['Start']) * multiplier
-    #                 metering_data['energy-end'] = self._stof(reading['Slut']) * multiplier
-    #                 metering_data['energy-used'] = self._stof(reading['Forbrug']) * multiplier
-    #                 metering_data['energy-exp-used'] = self._stof(fl['ForventetForbrugENG1']) * multiplier
-    #                 metering_data['energy-exp-end'] = self._stof(fl['ForventetAflaesningENG1']) * multiplier
-    #             elif reading['IndexNavn'] == "M3":
-    #                 metering_data['water-start'] = self._stof(reading['Start'])
-    #                 metering_data['water-end'] = self._stof(reading['Slut'])
-    #                 metering_data['water-used'] = self._stof(reading['Forbrug'])
-    #                 metering_data['water-exp-used'] = self._stof(fl['ForventetForbrugM3'])
-    #                 metering_data['water-exp-end'] = self._stof(fl['ForventetAflaesningM3'])
-    #             else:
-    #                 metering_data['extra-start'] = self._stof(reading['Start'])
-    #                 metering_data['extra-end'] = self._stof(reading['Slut'])
-    #                 metering_data['extra-used'] = self._stof(reading['Forbrug'])
-
-    #     # Because we are fetching data from the full year (or so far)
-    #     # The date is generated internally to be todays day of course.
-    #     date = datetime.now()
-    # Fake data testing:
-#        metering_data['temp-forward'] = random.randint(0, 100)
-#        metering_data['temp-return'] = random.randint(0, 100)
-#        metering_data['temp-exp-return'] = random.randint(0, 100)
-#        metering_data['temp-cooling'] = random.randint(0, 100)
-#        metering_data['energy-start'] = random.randint(0, 100)
-#        metering_data['energy-end'] = random.randint(0, 100)
-#        metering_data['energy-used'] = random.randint(0, 100)
-#        metering_data['energy-exp-used'] = random.randint(0, 100)
-#        metering_data['energy-exp-end'] = random.randint(0, 100)
-#        metering_data['water-start'] = random.randint(0, 100)
-#        metering_data['water-end'] = random.randint(0, 100)
-#        metering_data['water-used'] = random.randint(0, 100)
-#        metering_data['water-exp-used'] = random.randint(0, 100)
-#        metering_data['water-exp-end'] = random.randint(0, 100)
-#        date = datetime.strptime("2020-01-28T14:45:12Z", '%Y-%m-%dT%H:%M:%SZ')
-
-        # time_series = TimeSeries(200, date, metering_data)
-        # parsed_result[date] = time_series
-        # _LOGGER.debug(f"Done parsing results")
-        # return parsed_result
-
     def read_latest_measurements(self):
         reports = []
         i = 0

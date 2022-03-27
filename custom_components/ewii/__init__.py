629bd13d3b5d9825242ccbf257a182fca6d1f035
Raakilde <j.olesen@vindinggaard.dk>
2022-03-27 05:13:20 +0200

    Retry

--- custom_components/ewii/__init__.py
+++ custom_components/ewii/__init__.py
@@ -25,8 +25,8 @@ PLATFORMS = ["sensor"]
 # Every 6 hours seems appropriate to get an update ready in the morning
 # MIN_TIME_BETWEEN_UPDATES = timedelta(seconds=5)
 # Sure, let's bash the API service.. But useful when trying to get results fast.
-MIN_TIME_BETWEEN_UPDATES = timedelta(hours==6)
-
+# MIN_TIME_BETWEEN_UPDATES = timedelta(minutes=1)
+MIN_TIME_BETWEEN_UPDATES = timedelta(hours=6)
 
 async def async_setup(hass: HomeAssistant, config: dict):
     """Set up the Ewii component."""
--- custom_components/ewii/pyewii/__init__.py
+++ custom_components/ewii/pyewii/__init__.py
@@ -3,6 +3,6 @@ Init file for pyewii
 """
 from .ewii import Ewii
 from .models import TimeSeries
-from .models import RawResponse
+from .models import RawMeterData
 
 __version__ = "0.5.0"
